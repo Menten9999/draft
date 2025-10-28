7dadfe80e151dd43f22f6b1f413e04558dddc375
Menten9999 <haru.menten.michaelis@gmail.com>
2025-10-28 13:03:01 +0900

    fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-    const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyBwlnFf-ocmnNq3ZvudKC1Okgak6thaE9o",
   authDomain: "draft-59823.firebaseapp.com",
   databaseURL: "https://draft-59823-default-rtdb.firebaseio.com",
@@ -6,4 +6,4 @@
   storageBucket: "draft-59823.firebasestorage.app",
   messagingSenderId: "500987145047",
   appId: "1:500987145047:web:7e948b8ae4493f7e84ac12"
-    };
+};
